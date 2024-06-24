ca01f5ce6684c41aca44838561d6b899c683cba4
root <root@torr20-station.>
2024-06-24 12:04:47 +0600

    codecrafters submit [skip ci]

--- src/server.cpp
+++ src/server.cpp
@@ -8,6 +8,7 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <vector>
+#include <thread>
 using namespace std;
 
 vector<string> string_split(string &s, char delim, int len = 1)
@@ -23,6 +24,42 @@ vector<string> string_split(string &s, char delim, int len = 1)
   }
   tokens.push_back(s.substr(start));
   return tokens;
+}
+
+void handle_client(int client)
+{
+  char read_buffer[512] = {0};
+  int readbytes = read(client, read_buffer, 512);
+  if (readbytes < 0)
+  {
+    cerr << "read failed\n";
+    return;
+  }
+  cout << "read" << readbytes << " bytes " << endl;
+
+  string temp(read_buffer);
+  vector<string> requestparts = string_split(temp, '\r', 2);
+  vector<string> tokens = string_split(requestparts[0], '/');
+  string s;
+  if (tokens[1] == " HTTP")
+    s = "HTTP/1.1 200 OK\r\n\r\n";
+  else if (tokens[1] == "echo")
+  {
+    int len = tokens[2].length() - 5;
+    s = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + to_string(len) + "\r\n\r\n" + tokens[2].substr(0, len);
+  }
+  else if (tokens[1] == "user-agent HTTP")
+  {
+    tokens.clear();
+    tokens = string_split(requestparts[requestparts.size() - 3], ' '); // last 2 are empty, +1 for 0 index
+    s = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + to_string(tokens[1].length()) + "\r\n\r\n" + tokens[1];
+  }
+  else
+    s = "HTTP/1.1 404 Not Found\r\n\r\n";
+
+  char write_buffer[512] = {0};
+  strcpy(write_buffer, s.c_str());
+  write(client, write_buffer, strlen(write_buffer));
 }
 
 int main(int argc, char **argv)
@@ -74,42 +111,13 @@ int main(int argc, char **argv)
   int client_addr_len = sizeof(client_addr);
 
   std::cout << "Waiting for a client to connect...\n";
-
-  int client = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
-  std::cout << "Client connected\n";
-
-  char read_buffer[512] = {0};
-  int readbytes = read(client, read_buffer, 512);
-  if (readbytes < 0)
+  while (true)
   {
-    cerr << "read failed\n";
-    return 1;
+    int client = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
+    std::cout << "Client connected\n";
+    thread th(handle_client, client);
+    th.detach();
   }
-  cout << "read" << readbytes << " bytes " << endl;
-
-  string temp(read_buffer);
-  vector<string> requestparts = string_split(temp, '\r', 2);
-  vector<string> tokens = string_split(requestparts[0], '/');
-  string s;
-  if (tokens[1] == " HTTP")
-    s = "HTTP/1.1 200 OK\r\n\r\n";
-  else if (tokens[1] == "echo")
-  {
-    int len = tokens[2].length() - 5;
-    s = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + to_string(len) + "\r\n\r\n" + tokens[2].substr(0, len);
-  }
-  else if (tokens[1] == "user-agent HTTP")
-  {
-    tokens.clear();
-    tokens = string_split(requestparts[requestparts.size() - 3], ' '); // last 2 are empty, +1 for 0 index
-    s = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + to_string(tokens[1].length()) + "\r\n\r\n" + tokens[1];
-  }
-  else
-    s = "HTTP/1.1 404 Not Found\r\n\r\n";
-
-  char write_buffer[512] = {0};
-  strcpy(write_buffer, s.c_str());
-  write(client, write_buffer, strlen(write_buffer));
 
   close(server_fd);
   return 0;
